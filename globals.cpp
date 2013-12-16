96acd16eaac4921f6ed591bd569daf92ec43d253
Whales <fivedozenwhales@gmail.com>
2013-12-16 14:24:07 -0600

    Move data files to data folder

--- globals.cpp
+++ globals.cpp
@@ -7,6 +7,6 @@ Data_pool<World_terrain>  WORLD_TERRAIN;
 
 void load_global_data()
 {
-  TERRAIN.load_from("terrain.dat");
-  WORLD_TERRAIN.load_from("world_terrain.dat");
+  TERRAIN.load_from("data/terrain.dat");
+  WORLD_TERRAIN.load_from("data/world_terrain.dat");
 }
