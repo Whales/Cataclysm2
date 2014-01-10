6756fdac7fb3fa786a9354cea31db5bead67981e
Whales <fivedozenwhales@gmail.com>
2014-01-10 16:07:43 -0600

    Pathfinder class - does stupid zombie pathing and A* right now.

---
+++ pathfind.cpp
@@ -0,0 +1,420 @@
+#include "pathfind.h"
+#include "rng.h"
+#include <math.h>
+#include <algorithm>
+
+Path::Path()
+{
+  total_cost = 0;
+}
+
+Path::~Path()
+{
+}
+
+std::vector<Point> Path::get_points()
+{
+  return path;
+}
+
+int Path::get_cost()
+{
+  return total_cost;
+}
+
+Point Path::step(int n)
+{
+  if (n < 0 || n >= path.size()) {
+    return Point(-1, -1);
+  }
+  return path[n];
+}
+
+Point Path::operator[](int n)
+{
+  return step(n);
+}
+
+void Path::add_step(Point p, int cost)
+{
+  path.push_back(p);
+  total_cost += cost;
+}
+
+void Path::reverse()
+{
+  std::reverse(path.begin(), path.end());
+}
+
+Generic_map::Generic_map(int x, int y)
+{
+  set_size(x, y);
+}
+
+Generic_map::~Generic_map()
+{
+}
+
+void Generic_map::set_size(int x, int y)
+{
+  cost.clear();
+  if (x == 0 && y == 0) {
+    return;
+  }
+  std::vector<int> tmpvec;
+  for (int i = 0; i < y; i++) {
+    tmpvec.push_back(-1);
+  }
+  for (int i = 0; i < x; i++) {
+    cost.push_back( tmpvec );
+  }
+}
+
+int Generic_map::get_size_x()
+{
+  return cost.size();
+}
+
+int Generic_map::get_size_y()
+{
+  if (cost.empty()) {
+    return 0;
+  }
+  return cost[0].size();
+}
+
+int Generic_map::get_cost(int x, int y)
+{
+  if (x < 0 || x >= get_size_x() || y < 0 || y >= get_size_y()) {
+    return -1;
+  }
+  return cost[x][y];
+}
+
+int Generic_map::get_cost(Point p)
+{
+  return get_cost(p.x, p.y);
+}
+
+bool Generic_map::blocked(int x, int y)
+{
+  return (get_cost(x, y) >= 0);
+}
+
+bool Generic_map::blocked(Point p)
+{
+  return blocked(p.x, p.y);
+}
+
+Pathfinder::Pathfinder()
+{
+  set_bounds();
+}
+
+Pathfinder::Pathfinder(Generic_map m)
+{
+  set_map(m);
+}
+
+Pathfinder::~Pathfinder()
+{
+}
+
+void Pathfinder::set_map(Generic_map m)
+{
+  map = m;
+  set_bounds();
+}
+
+void Pathfinder::set_bounds(int x0, int y0, int x1, int y1)
+{
+  if (x0 < 0) {
+    x0 = 0;
+  }
+  if (x1 <= 0 || x1 >= map.get_size_x()) {
+    x1 = map.get_size_x() - 1;
+  }
+  if (x0 > x1) {
+    int tmp = x1;
+    x1 = x0;
+    x0 = tmp;
+  }
+  if (y0 < 0) {
+    y0 = 0;
+  }
+  if (y1 <= 0 || y1 >= map.get_size_y()) {
+    y1 = map.get_size_y() - 1;
+  }
+  if (y0 > y1) {
+    int tmp = y1;
+    y1 = y0;
+    y0 = tmp;
+  }
+}
+
+void Pathfinder::set_bounds(Point p0, Point p1)
+{
+  set_bounds(p0.x, p0.y, p1.x, p1.y);
+}
+
+bool Pathfinder::in_bounds(int x, int y)
+{
+  return (x >= x_min && x <= x_max && y >= y_min && y <= y_max);
+}
+
+bool Pathfinder::in_bounds(Point p)
+{
+  return in_bounds(p.x, p.y);
+}
+
+Path Pathfinder::get_path(Path_type type, int x0, int y0, int x1, int y1)
+{
+  return get_path(type, Point(x0, y0), Point(x1, y1));
+}
+
+Path Pathfinder::get_path(Path_type type, Point start, Point end)
+{
+  switch (type) {
+    case PATH_NULL:
+    case PATH_LINE:
+      return path_line(start, end);
+    case PATH_A_STAR:
+      return path_a_star(start, end);
+    default:
+      return path_line(start, end);
+  }
+  return path_line(start, end);
+}
+
+Path Pathfinder::path_line(Point start, Point end)
+{
+  Path ret;
+  Point cur = start;
+  bool done = false;
+  while (!done) {
+    Point options[5];
+    for (int i = 0; i < 5; i++) {
+      options[i] = cur;
+    }
+    bool x_diff_bigger = ( abs(end.x - cur.x) > abs(end.y - cur.y) );
+    if (end.x > cur.x) {
+      if (end.y > cur.y) {
+        options[0] = Point(cur.x + 1, cur.y + 1);
+        if (x_diff_bigger) {
+          options[1] = Point(cur.x + 1, cur.y    );
+          options[2] = Point(cur.x    , cur.y + 1);
+          options[3] = Point(cur.x + 1, cur.y - 1);
+          options[4] = Point(cur.x - 1, cur.y + 1);
+        } else {
+          options[1] = Point(cur.x    , cur.y + 1);
+          options[2] = Point(cur.x + 1, cur.y    );
+          options[3] = Point(cur.x + 1, cur.y - 1);
+          options[4] = Point(cur.x - 1, cur.y + 1);
+        }
+      } else if (end.y < cur.y) {
+        options[0] = Point(cur.x + 1, cur.y - 1);
+        if (x_diff_bigger) {
+          options[1] = Point(cur.x + 1, cur.y    );
+          options[2] = Point(cur.x    , cur.y - 1);
+          options[3] = Point(cur.x + 1, cur.y + 1);
+          options[4] = Point(cur.x - 1, cur.y - 1);
+        } else {
+          options[1] = Point(cur.x    , cur.y - 1);
+          options[2] = Point(cur.x + 1, cur.y    );
+          options[3] = Point(cur.x - 1, cur.y - 1);
+          options[4] = Point(cur.x + 1, cur.y + 1);
+        }
+      } else { // (end.y == cur.y)
+        options[0] = Point(cur.x + 1, cur.y    );
+        if (one_in(2)) {
+          options[1] = Point(cur.x + 1, cur.y - 1);
+          options[2] = Point(cur.x + 1, cur.y + 1);
+          options[3] = Point(cur.x    , cur.y - 1);
+          options[4] = Point(cur.x    , cur.y + 1);
+        } else {
+          options[1] = Point(cur.x + 1, cur.y + 1);
+          options[2] = Point(cur.x + 1, cur.y - 1);
+          options[3] = Point(cur.x    , cur.y + 1);
+          options[4] = Point(cur.x    , cur.y - 1);
+        }
+      }
+    } else if (end.x < cur.x) {
+      if (end.y > cur.y) {
+        options[0] = Point(cur.x - 1, cur.y + 1);
+        if (x_diff_bigger) {
+          options[1] = Point(cur.x - 1, cur.y    );
+          options[2] = Point(cur.x    , cur.y + 1);
+          options[3] = Point(cur.x - 1, cur.y - 1);
+          options[4] = Point(cur.x + 1, cur.y + 1);
+        } else {
+          options[1] = Point(cur.x    , cur.y + 1);
+          options[2] = Point(cur.x - 1, cur.y    );
+          options[3] = Point(cur.x - 1, cur.y - 1);
+          options[4] = Point(cur.x + 1, cur.y + 1);
+        }
+      } else if (end.y < cur.y) {
+        options[0] = Point(cur.x - 1, cur.y - 1);
+        if (x_diff_bigger) {
+          options[1] = Point(cur.x - 1, cur.y    );
+          options[2] = Point(cur.x    , cur.y - 1);
+          options[3] = Point(cur.x - 1, cur.y + 1);
+          options[4] = Point(cur.x + 1, cur.y - 1);
+        } else {
+          options[1] = Point(cur.x    , cur.y - 1);
+          options[2] = Point(cur.x - 1, cur.y    );
+          options[3] = Point(cur.x + 1, cur.y - 1);
+          options[4] = Point(cur.x - 1, cur.y + 1);
+        }
+      } else { // (end.y == cur.y)
+        options[0] = Point(cur.x - 1, cur.y    );
+        if (one_in(2)) {
+          options[1] = Point(cur.x - 1, cur.y - 1);
+          options[2] = Point(cur.x - 1, cur.y + 1);
+          options[3] = Point(cur.x    , cur.y - 1);
+          options[4] = Point(cur.x    , cur.y + 1);
+        } else {
+          options[1] = Point(cur.x - 1, cur.y + 1);
+          options[2] = Point(cur.x - 1, cur.y - 1);
+          options[3] = Point(cur.x    , cur.y + 1);
+          options[4] = Point(cur.x    , cur.y - 1);
+        }
+      }
+    } else { // (end.x == cur.x)
+      if (end.y > cur.y) {
+        options[0] = Point(cur.x    , cur.y + 1);
+        if (one_in(2)) {
+          options[1] = Point(cur.x + 1, cur.y + 1);
+          options[2] = Point(cur.x - 1, cur.y + 1);
+          options[3] = Point(cur.x + 1, cur.y    );
+          options[4] = Point(cur.x - 1, cur.y    );
+        } else {
+          options[1] = Point(cur.x - 1, cur.y + 1);
+          options[2] = Point(cur.x + 1, cur.y + 1);
+          options[3] = Point(cur.x - 1, cur.y    );
+          options[4] = Point(cur.x + 1, cur.y    );
+        }
+      } else if (end.y < cur.y) {
+        options[0] = Point(cur.x    , cur.y - 1);
+        if (one_in(2)) {
+          options[1] = Point(cur.x + 1, cur.y - 1);
+          options[2] = Point(cur.x - 1, cur.y - 1);
+          options[3] = Point(cur.x + 1, cur.y    );
+          options[4] = Point(cur.x - 1, cur.y    );
+        } else {
+          options[1] = Point(cur.x - 1, cur.y - 1);
+          options[2] = Point(cur.x + 1, cur.y - 1);
+          options[3] = Point(cur.x - 1, cur.y    );
+          options[4] = Point(cur.x + 1, cur.y    );
+        }
+      } else { // (end.y == cur.y)
+        done = true; // We're finished
+      }
+    }
+    bool picked_next = false;
+    for (int i = 0; i < 5 && !picked_next; i++) {
+      if (!map.blocked( options[i] ) && in_bounds( options[i] )) {
+        picked_next = true;
+        cur = options[i];
+      }
+    }
+    if (!picked_next) { // Couldn't reach our target using this stupid algo!
+      done = true;
+    } else {
+      ret.add_step(cur, map.get_cost(cur));
+    }
+  } // while (!done)
+
+  if (cur != end) { // We didn't make it :(
+    return Path();
+  }
+  return ret;
+}
+
+
+enum A_star_status
+{
+  ASTAR_NONE,
+  ASTAR_OPEN,
+  ASTAR_CLOSED
+};
+
+Path Pathfinder::path_a_star(Point start, Point end)
+{
+  int x_size = map.get_size_x();
+  int y_size = map.get_size_y();
+
+  std::vector<Point> open_points;
+  A_star_status status[x_size][y_size];
+  int   gscore[x_size][y_size];
+  int   hscore[x_size][y_size];
+  Point parent[x_size][y_size];
+
+// Init everything to 0
+  for (int x = 0; x < x_size; x++) {
+    for (int y = 0; y < y_size; y++) {
+      status[x][y] = ASTAR_NONE;
+      gscore[x][y] = 0;
+      hscore[x][y] = 0;
+      parent[x][y] = Point(-1, -1);
+    }
+  }
+
+  status[start.x][start.y] = ASTAR_OPEN;
+  open_points.push_back(start);
+
+  bool done = false;
+
+  while (!done && !open_points.empty()) {
+// 1) Find the lowest cost in open_points:
+    int lowest_cost = -1, point_index = -1;
+    Point current;
+    for (int i = 0; i < open_points.size(); i++) {
+      Point p = open_points[i];
+      int score = gscore[p.x][p.y] + hscore[p.x][p.y];
+      if (i == 0 || score < lowest_cost) {
+        lowest_cost = score;
+        current = p;
+        point_index = i;
+      }
+    }
+// 2) Check if that's the endpoint
+    if (current == end) {
+      done = true;
+    } else {
+// 3) Set that point to be closed
+      open_points.erase(open_points.begin() + point_index);
+      status[current.x][current.y] = ASTAR_CLOSED;
+// 4) Examine all adjacent points
+      for (int x = current.x - 1; x <= current.x + 1; x++) {
+        for (int y = current.y - 1; y <= current.y + 1; y++) {
+// If it's in-bounds and not blocked...
+          if (in_bounds(x, y) && !map.blocked(x, y)) {
+            int g = map.get_cost(x, y);
+// If it's unexamined, make it open and set its values
+            if (status[x][y] == ASTAR_NONE) {
+              status[x][y] = ASTAR_OPEN;
+              gscore[x][y] = g;
+              hscore[x][y] = rl_dist(x, y, end.x, end.y);
+              parent[x][y] = current;
+              open_points.push_back( Point(x, y) );
+// If it's open and we're a better parent, make us the parent
+            } else if (status[x][y] == ASTAR_OPEN && g < gscore[x][y]) {
+              gscore[x][y] = g;
+              parent[x][y] = current;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  Path ret;
+  Point cur = end;
+  ret.add_step(cur, map.get_cost(cur));
+  while (parent[cur.x][cur.y] != start) {
+    cur = parent[cur.x][cur.y];
+    ret.add_step(cur, map.get_cost(cur));
+  }
+  ret.reverse();
+  return ret;
+}
---
+++ pathfind.h
@@ -0,0 +1,77 @@
+#ifndef _PATHFIND_H_
+#define _PATHFIND_H_
+
+#include "geometry.h"
+#include <vector>
+
+enum Path_type
+{
+  PATH_NULL = 0,
+  PATH_LINE,
+  PATH_A_STAR,
+};
+
+class Path
+{
+public:
+  Path();
+  ~Path();
+
+  std::vector<Point> get_points();
+  int get_cost();
+  Point step(int n);
+  Point operator[](int n);
+
+  void add_step(Point p, int cost);
+  void reverse();
+private:
+  std::vector<Point> path;
+  int total_cost;
+};
+
+class Generic_map
+{
+public:
+  Generic_map(int x = 0, int y = 0);
+  ~Generic_map();
+
+  void set_size(int x, int y);
+  void set_cost(int x, int y);
+
+  int  get_size_x();
+  int  get_size_y();
+  int  get_cost(int x, int y);
+  int  get_cost(Point p);
+  bool blocked(int x, int y);
+  bool blocked(Point p);
+private:
+  std::vector<std::vector<int> > cost;
+};
+
+class Pathfinder
+{
+public:
+  Pathfinder();
+  Pathfinder(Generic_map m);
+  ~Pathfinder();
+
+  void set_map(Generic_map m);
+
+  void set_bounds(int x0 = -1, int y0 = -1, int x1 = -1, int y1 = -1);
+  void set_bounds(Point p0, Point p1);
+
+  bool in_bounds(int x, int y);
+  bool in_bounds(Point p);
+  
+  Path get_path(Path_type type, int x0, int y0, int x1, int y1);
+  Path get_path(Path_type type, Point start, Point end);
+
+private:
+  Generic_map map;
+  int x_min, x_max, y_min, y_max;
+
+  Path path_line(Point start, Point end);
+  Path path_a_star(Point start, Point end);
+};
+
+#endif
