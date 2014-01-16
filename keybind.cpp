b705cd8b8df3c5a770f2b326a23014dc59d986bc
Whales <fivedozenwhales@gmail.com>
2014-01-16 10:04:49 -0600

    Keybindings for throw/fire.

--- keybind.cpp
+++ keybind.cpp
@@ -90,6 +90,8 @@ std::string interface_action_name(Interface_action action)
     case IACTION_WEAR:                    return "wear";
     case IACTION_RELOAD_EQUIPPED:         return "reload_equipped";
     case IACTION_RELOAD:                  return "reload";
+    case IACTION_THROW:                   return "throw";
+    case IACTION_FIRE:                    return "fire";
     case IACTION_MESSAGES_SCROLL_BACK:    return "messages_scroll_back";
     case IACTION_MESSAGES_SCROLL_FORWARD: return "messages_scroll_forward";
     case IACTION_VIEW_WORLDMAP:           return "view_worldmap";
