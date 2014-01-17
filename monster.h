8fa2fbe2ad1a1991b66740788022055ad5fcf692
Whales <fivedozenwhales@gmail.com>
2014-01-17 10:25:15 -0600

    Remove Monster_pool.

--- monster.h
+++ monster.h
@@ -50,31 +50,4 @@ private:
   int wander_duration;
 };
 
-/* For now, Monster_pool does NOT include a map which uses location as a key.
- * In order for this map to be useful, we'd have to update it every turn, which
- * means it' probably be more trouble than it's worth, except when the map is
- * being called several times per turn.  We'd also have to update it after
- * every monster moves, which is a lot.
- * This means that monster_at() has to iterate over all monsters, which is
- * potentially slow, but what can you do.
- */
-
-class Monster_pool
-{
-public:
-  Monster_pool();
-  ~Monster_pool();
-
-  void add_monster(Monster* mon);
-
-  Monster* lookup_uid(int uid);
-  Monster* monster_at(int posx, int posy);
-
-  std::list<Monster*> instances;
-private:
-  std::map<int,Monster*> uid_map;
-  int next_uid;
-};
-  
-
 #endif
