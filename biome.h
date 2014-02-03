7ece3fee67092fed45fc3631545e87018f6b1162
Whales <fivedozenwhales@gmail.com>
2014-02-03 12:51:49 +0000

    Changed biome default terrain weight to 100, updated data accordingly.

--- biome.h
+++ biome.h
@@ -8,7 +8,7 @@
 
 struct World_terrain_chance
 {
-  World_terrain_chance(int C = 10, World_terrain* WT = NULL) :
+  World_terrain_chance(int C = 100, World_terrain* WT = NULL) :
     chance (C), terrain (WT) {}
   int chance;
   World_terrain* terrain;
