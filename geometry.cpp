713e7d8d44b722808e0b1af2bdbf1db82216a45a
Whales <fivedozenwhales@gmail.com>
2014-04-09 17:18:02 +0000

    Better indentation.

--- geometry.cpp
+++ geometry.cpp
@@ -8,59 +8,59 @@
 
 std::vector <Point> line_to(int x0, int y0, int x1, int y1)
 {
- int t = 0;
- std::vector<Point> ret;
- int dx = x1 - x0;
- int dy = y1 - y0;
- int ax = abs(dx)<<1;
- int ay = abs(dy)<<1;
- int sx = SGN(dx);
- int sy = SGN(dy);
- if (dy == 0) sy = 0;
- if (dx == 0) sx = 0;
- Point cur;
- cur.x = x0;
- cur.y = y0;
+  int t = 0;
+  std::vector<Point> ret;
+  int dx = x1 - x0;
+  int dy = y1 - y0;
+  int ax = abs(dx)<<1;
+  int ay = abs(dy)<<1;
+  int sx = SGN(dx);
+  int sy = SGN(dy);
+  if (dy == 0) sy = 0;
+  if (dx == 0) sx = 0;
+  Point cur;
+  cur.x = x0;
+  cur.y = y0;
 
- int xmin = (x0 < x1 ? x0 : x1), ymin = (y0 < y1 ? y0 : y1),
-     xmax = (x0 > x1 ? x0 : x1), ymax = (y0 > y1 ? y0 : y1);
+  int xmin = (x0 < x1 ? x0 : x1), ymin = (y0 < y1 ? y0 : y1),
+      xmax = (x0 > x1 ? x0 : x1), ymax = (y0 > y1 ? y0 : y1);
 
- xmin -= abs(dx);
- ymin -= abs(dy);
- xmax += abs(dx);
- ymax += abs(dy);
+  xmin -= abs(dx);
+  ymin -= abs(dy);
+  xmax += abs(dx);
+  ymax += abs(dy);
 
- if (ax == ay) {
-  do {
-   cur.y += sy;
-   cur.x += sx;
-   ret.push_back(cur);
-  } while ((cur.x != x1 || cur.y != y1) &&
-           (cur.x >= xmin && cur.x <= xmax && cur.y >= ymin && cur.y <= ymax));
- } else if (ax > ay) {
-  do {
-   if (t > 0) {
-    cur.y += sy;
-    t -= ax;
-   }
-   cur.x += sx;
-   t += ay;
-   ret.push_back(cur);
-  } while ((cur.x != x1 || cur.y != y1) &&
-           (cur.x >= xmin && cur.x <= xmax && cur.y >= ymin && cur.y <= ymax));
- } else {
-  do {
-   if (t > 0) {
-    cur.x += sx;
-    t -= ay;
-   }
-   cur.y += sy;
-   t += ax;
-   ret.push_back(cur);
-  } while ((cur.x != x1 || cur.y != y1) &&
-           (cur.x >= xmin && cur.x <= xmax && cur.y >= ymin && cur.y <= ymax));
- }
- return ret;
+  if (ax == ay) {
+    do {
+      cur.y += sy;
+      cur.x += sx;
+      ret.push_back(cur);
+    } while ((cur.x != x1 || cur.y != y1) &&
+            (cur.x >= xmin && cur.x <= xmax && cur.y >= ymin && cur.y <= ymax));
+  } else if (ax > ay) {
+    do {
+      if (t > 0) {
+        cur.y += sy;
+        t -= ax;
+      }
+      cur.x += sx;
+      t += ay;
+      ret.push_back(cur);
+    } while ((cur.x != x1 || cur.y != y1) &&
+            (cur.x >= xmin && cur.x <= xmax && cur.y >= ymin && cur.y <= ymax));
+  } else {
+    do {
+      if (t > 0) {
+        cur.x += sx;
+        t -= ay;
+      }
+      cur.y += sy;
+      t += ax;
+      ret.push_back(cur);
+    } while ((cur.x != x1 || cur.y != y1) &&
+            (cur.x >= xmin && cur.x <= xmax && cur.y >= ymin && cur.y <= ymax));
+  }
+  return ret;
 }
 
 std::vector<Point> line_to(Point origin, Point target)
