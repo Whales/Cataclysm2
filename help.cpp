93ffeb526409d0e1579dbfe30cf7812c92bbb13f
Whales <fivedozenwhales@gmail.com>
2014-04-13 15:27:02 +0000

    Updated help interface, wrote some help text files.

--- help.cpp
+++ help.cpp
@@ -8,6 +8,7 @@ void help_screen()
 // This is a list of text files bound to different number input.
 // They'll be looked for in DATA_DIR/help/
   std::vector<std::string> help_files;
+  help_files.push_back("story.txt");
   help_files.push_back("introduction.txt");
   help_files.push_back("interface.txt");
   help_files.push_back("movement.txt");
@@ -26,8 +27,11 @@ void help_screen()
   while (!done) {
     i_help.draw(&w_help);
     long ch = getch();
-    if (ch >= '1' && ch - '1' < help_files.size()) {
-      std::string filename = DATA_DIR + "/help/" + help_files[ch - '1'];
+    if (ch >= 'A' && ch <= 'Z') {
+      ch = ch - 'A' + 'a';
+    }
+    if (ch >= 'a' && ch - 'a' < help_files.size()) {
+      std::string filename = DATA_DIR + "/help/" + help_files[ch - 'a'];
       if (!file_exists(filename)) {
         i_help.set_data("text_help",
                         "Help file '" + filename + "' doesn't exist.");
