779a103ab929df85dcf6d7f8da0874b755525467
Whales <fivedozenwhales@gmail.com>
2014-07-24 01:16:53 +0000

    Add a new property to Monster_ability_fields, affect_self.  If true, then fields may be placed underneath the monster.

--- monster_ability.h
+++ monster_ability.h
@@ -160,7 +160,8 @@ struct Monster_ability_fields
   virtual bool effect(Monster* user);
 
   int range;  // Radius of affected tiles; defaults to 1
-  bool affect_all_tiles;  // If true, all tiles affected; defaulst to false
+  bool affect_all_tiles;  // If true, all tiles affected; defaults to false
+  bool affect_self; // If true, affect tile under monster; defaults to false
   Dice tiles_affected;  // How many tiles to hit; ignored if <affect_all_tiles>
   Variable_string field_type; // Type of field (maybe random)
   Dice duration;  // Duration of field(s) generated; if <0 rolled, no field made
