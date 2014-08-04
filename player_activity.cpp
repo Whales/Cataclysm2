203833a4694bcec4c42313ebd7e062d51ca4d954
Whales <fivedozenwhales@gmail.com>
2014-08-04 00:36:24 +0000

    New activity - PLAYER_ACTIVITY_READ

--- player_activity.cpp
+++ player_activity.cpp
@@ -16,7 +16,9 @@ std::string get_activity_name(Player_activity_type type)
     case PLAYER_ACTIVITY_NULL:    return "nothing";
     case PLAYER_ACTIVITY_WAIT:    return "wait";
     case PLAYER_ACTIVITY_RELOAD:  return "reload";
+    case PLAYER_ACTIVITY_READ:    return "read";
     case PLAYER_ACTIVITY_MAX:     return "BUG - PLAYER_ACTIVITY_MAX";
+
     default:                      return "BUG - Unnamed Player_activity_type";
   }
   return "BUG - Escaped switch (get_activity_name())";
@@ -28,7 +30,9 @@ std::string get_activity_participle(Player_activity_type type)
     case PLAYER_ACTIVITY_NULL:    return "doing nothing";
     case PLAYER_ACTIVITY_WAIT:    return "waiting";
     case PLAYER_ACTIVITY_RELOAD:  return "reloading";
+    case PLAYER_ACTIVITY_READ:    return "reading";
     case PLAYER_ACTIVITY_MAX:     return "BUG - PLAYER_ACTIVITY_MAX";
+
     default:                   return "BUG - Unparticiple Player_activity_type";
   }
   return "BUG - Escaped switch (get_activity_participle())";
--- player_activity.h
+++ player_activity.h
@@ -9,11 +9,12 @@ enum Player_activity_type
   PLAYER_ACTIVITY_NULL,
   PLAYER_ACTIVITY_WAIT,
   PLAYER_ACTIVITY_RELOAD,
+  PLAYER_ACTIVITY_READ,
   PLAYER_ACTIVITY_MAX
 };
 
-std::string get_activity_name      (Player_activity_type type);
-std::string get_activity_participle(Player_activity_type type);
+std::string get_activity_name       (Player_activity_type type);
+std::string get_activity_participle (Player_activity_type type);
 
 struct Player_activity
 {
