e6ebeb8de65765c1f46950dbf1810f55cb13482e
Whales <fivedozenwhales@gmail.com>
2014-08-12 02:26:13 +0000

    Mission_template loading and names for Mission_type.

--- mission.cpp
+++ mission.cpp
@@ -1,6 +1,7 @@
 #include "mission.h"
 #include "rng.h"
 #include "game.h"
+#include <sstream>
 
 Mission::Mission(Mission_type T, std::string T_N, int X, Time D)
 {
@@ -11,6 +12,84 @@ Mission::Mission(Mission_type T, std::string T_N, int X, Time D)
   if (deadline.get_turn() == -1) {
     deadline = GAME.time + HOURS(1);
   }
+}
+
+void Mission_template::assign_uid(int ID)
+{
+  uid = ID;
+}
+
+std::string Mission_template::get_data_name()
+{
+  std::stringstream ret;
+  ret << mission_type_name(type) << "_" << target_name;
+  return ret.str();
+}
+
+std::string Mission_template::get_name()
+{
+  if (type == MISSION_NULL) {
+    return "NULL";
+  }
+
+  std::stringstream ret;
+  ret << mission_type_display_name(type) << " " << target_name;
+  return ret.str();
+}
+
+bool Mission_template::load_data(std::istream& data)
+{
+  std::string ident, junk;
+  while (ident != "done" && !data.eof()) {
+    if ( ! (data >> ident)) {
+      return false;
+    }
+    ident = no_caps(ident);
+
+    if (!ident.empty() && ident[0] == '#') {
+// It's a comment
+      std::getline(data, junk);
+
+    } else if (ident == "type:") {
+      std::string type_name;
+      std::getline(data, type_name);
+      type = lookup_mission_type(type_name);
+      if (type == MISSION_NULL) {
+        debugmsg("Unknown Mission_type '%s'", type_name.c_str());
+        return false;
+      }
+
+    } else if (ident == "target:") {
+      std::getline(data, target_name);
+      target_name = trim(target_name);
+      if (target_name.empty()) {
+        debugmsg("Empty target name (%s).", mission_type_name(type).c_str());
+        return false;
+      }
+
+    } else if (ident == "xp:") {
+      data >> xp_min >> xp_max;
+      if (xp_min < 0 || xp_max < xp_min) {
+        debugmsg("Invalid XP (%d to %d) (%s).", xp_min, xp_max,
+                 get_data_name().c_str());
+        return false;
+      }
+
+    } else if (ident == "time:") {
+      data >> time_min >> time_max;
+      if (time_min < 0 || time_max < time_min) {
+        debugmsg("Invalid time (%d to %d) (%s).", time_min, time_max,
+                 get_data_name().c_str());
+        return false;
+      }
+
+    } else if (ident != "done") {
+      debugmsg("Unknown Mission_template property '%s' (%s).", ident.c_str(),
+               get_name().c_str());
+    }
+  }
+
+  return true;
 }
 
 bool Mission::set_from_template(const Mission_template& temp)
@@ -27,4 +106,40 @@ bool Mission::set_from_template(const Mission_template& temp)
   deadline = GAME.time + HOURS(time_to_finish);
 
   return true;
+}
+
+Mission_type lookup_mission_type(std::string name)
+{
+  name = no_caps( trim( name ) );
+  for (int i = 0; i < MISSION_MAX; i++) {
+    Mission_type ret = Mission_type(i);
+    if (name == no_caps( mission_type_name(ret) )) {
+      return ret;
+    }
+  }
+  return MISSION_NULL;
+}
+
+std::string mission_type_name(Mission_type type)
+{
+  switch (type) {
+    case MISSION_NULL:        return "NULL";
+    case MISSION_EAT:         return "eat";
+    case MISSION_READ_GENRE:  return "read_genre";
+    case MISSION_MAX:         return "BUG - MISSION_MAX";
+    default:                  return "BUG - Unnamed Mission_type";
+  }
+  return "BUG - Escaped mission_type_name() switch";
+}
+
+std::string mission_type_display_name(Mission_type type)
+{
+  switch (type) {
+    case MISSION_NULL:        return "NULL";
+    case MISSION_EAT:         return "Eat";
+    case MISSION_READ_GENRE:  return "Read genre";
+    case MISSION_MAX:         return "BUG - MISSION_MAX";
+    default:                  return "BUG - Unnamed Mission_type";
+  }
+  return "BUG - Escaped mission_type_display_name() switch";
 }
--- mission.h
+++ mission.h
@@ -2,8 +2,8 @@
 #define _MISSION_H_
 
 #include "time.h"
-#include "globals.h"
 #include <string>
+#include <istream>
 
 enum Mission_type
 {
@@ -15,6 +15,7 @@ enum Mission_type
 
 Mission_type lookup_mission_type(std::string name);
 std::string mission_type_name(Mission_type type);
+std::string mission_type_display_name(Mission_type type);
 
 struct Mission_template
 {
@@ -22,8 +23,15 @@ struct Mission_template
                    int X_min = 0, int X_max = 0, int T_min = 1, int T_max = 1) :
     type (T), target_name (T_N), xp_min (X_min), xp_max (X_max),
     time_min (T_min), time_max (T_max)
-    { }
+    { uid = -1; }
 
+  void assign_uid(int ID);
+
+  std::string get_data_name();
+  std::string get_name(); // type + target_name
+  bool load_data(std::istream& data);
+
+  int uid;
   Mission_type type;
   std::string target_name;  // Can refer to a item, monster, genre, anything...
   int xp_min, xp_max; // Range of XP we get
